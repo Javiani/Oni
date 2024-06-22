5d95fafec64bb786543f39b6bf02571ac22738fe
Javiani <eduardo.ottaviani@gmail.com>
2024-06-22 02:12:34 -0300

    Fixing types

--- types.d.ts
+++ types.d.ts
@@ -1,15 +1,19 @@
 export declare type Subscription = (state: State, params: SubscriptionParams) => unknown;
-export declare type Action = (state: State, payload: object) => State;
+export declare type Action = (state: State, payload: any, store : Store) => State | void;
+
 export interface State {
     [key: string]: any;
 }
+
 export interface Actions {
     [key: string]: Action;
 }
+
 export interface SubscriptionParams {
     action: string;
     payload: object;
 }
+
 export interface Store {
     getState(): State;
     subscribe(fn: Subscription): Function;
@@ -17,6 +21,7 @@ export interface Store {
     patternMatch(mapfn: any): any;
     destroy(): void;
 }
+
 export default function Oni(initialState: State, actions: Actions): Store;
 
 interface useStoreInterface {
@@ -25,6 +30,7 @@ interface useStoreInterface {
     action: keyof Actions;
     dispatch(action: keyof Actions, payload?: object): Promise<unknown>;
 }
+
 export declare const createStore: (initialState: State, actions: Actions) => {
     store: Store;
     useStore(actions?: Array<keyof Actions>): useStoreInterface;
